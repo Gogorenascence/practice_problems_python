1aa7f4754f7b7e98d3695e885f8e6ed7e3f7bb01
Kornebari <nantahkl@gmail.com>
2024-04-10 09:34:31 -0500

    first commit

---
+++ problems/problem_025.py
@@ -0,0 +1,10 @@
+# Complete the calculate_sum function which accepts
+# a list of numerical values and returns the sum of
+# the numbers.
+#
+# If the list of values is empty, the function should
+# return None
+#
+
+def calculate_sum(values):
+    pass
---
+++ problems/problem_027.py
@@ -0,0 +1,8 @@
+# Complete the max_in_list function to find the
+# maximum value in a list
+#
+# If the list is empty, then return None.
+#
+
+def max_in_list(values):
+    pass
